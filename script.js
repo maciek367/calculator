66d3355d0db9f20254b45e24bf0f5d77185df385
maciek5000 <maciek2baba2@gmail.com>
2022-09-25 00:34:57 +0200

    split into functions, add functions that handle mathematics

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const resultDiv = document.getElementById("result-text")
+const input = document.getElementById("input")
+const previous = document.getElementById("previous")
 
 const clearButton = document.getElementById("clear")
 const deleteButton = document.getElementById("delete")
+
+clearButton.addEventListener("click", clear)
+deleteButton.addEventListener("click", removeOne)
 
 const buttons = [
 document.getElementById("zero"),
@@ -24,24 +28,65 @@ const divideButton = document.getElementById("divide")
 const multiplyButton = document.getElementById("multiply")
 const subtractButton = document.getElementById("subtract")
 const addButton = document.getElementById("add")
+
+divideButton.addEventListener("click", handleDivide)
+multiplyButton.addEventListener("click", handleMultiply)
+subtractButton.addEventListener("click", handleSubtract)
+addButton.addEventListener("click", handleAdd)
+
 const dotButton = document.getElementById("dot")
 const equalsButton = document.getElementById("equals")
 
-deleteButton.addEventListener("click", removeOne)
+dotButton.addEventListener("click", dot)
+equalsButton.addEventListener("click", equals)
+
+function clearInput() {
+  input.innerText = "0"
+}
+
+function pushToPrevious() {
+  previous.innerText += input.innerText
+}
+
+function handleAdd() {
+
+}
+
+function divide(a,b) {
+  if (b === 0) return "Can't divide by zero"
+  return (a / b).toFixed(3)
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function add(a,b) {
+  return a + b
+}
+
+function clear() {
+  previous.innerText = ""
+  input.innerText = "0"
+}
 
 function numberButtonClicked() {
-  if (resultDiv.innerText == "0") resultDiv.innerText = ""
+  if (input.innerText == "0") input.innerText = ""
 
-  resultDiv.innerText += this.innerText
+  input.innerText += this.innerText
 } 
 
 function removeOne() {
-  let text = resultDiv.innerText
-  if (text.length <= 1) return resultDiv.innerText = "0"
+  let text = input.innerText
+  if (text.length <= 1) return input.innerText = "0"
 
-  resultDiv.innerText = resultDiv.innerText.slice(0, -1)
+  input.innerText = input.innerText.slice(0, -1)
 }
 
 function showResult(result) {
-  resultDiv.innerText = result
+  input.innerText = result
 }
