442eaabd1513f3d82729a29e57823180c974ce15
maciek367 <maciek2baba2@gmail.com>
2022-11-30 22:36:40 +0100

    fix issue: equals worked when input was empty, add keyboard support, refactor code

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const previous = document.getElementById("previous")
 let previousNum = null
 let equalsBlocked = false
 
+// TODO add keyboard support
+
 const clearButton = document.getElementById("clear")
 const deleteButton = document.getElementById("delete")
 
@@ -23,8 +25,10 @@ document.getElementById("eight"),
 document.getElementById("nine")
 ]
 
+document.addEventListener("keydown", (e) => keyPressed(e))
+
 buttons.forEach(button => {
-  button.addEventListener("click", numberButtonClicked)
+  button.addEventListener("click", (e) => numberButtonClicked(e.target.innerText))
 })
 
 const divideButton = document.getElementById("divide")
@@ -40,19 +44,63 @@ addButton.addEventListener("click", handleAdd)
 const dotButton = document.getElementById("dot")
 const equalsButton = document.getElementById("equals")
 
-dotButton.addEventListener("click", dot)
+dotButton.addEventListener("click", handleDot)
 equalsButton.addEventListener("click", equals)
+
+function keyPressed(event) {
+  let keyCode = event.key
+  switch(keyCode) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      numberButtonClicked(keyCode)
+      break
+    case ".":
+      handleDot()
+      break
+    case "*":
+      handleMultiply()
+      break
+    case "/":
+      event.preventDefault()
+      handleDivide()
+      break
+    case "-":
+      handleSubtract()
+      break
+    case "+":
+      handleAdd()
+      break
+    case "=":
+      equals()
+      break
+    case "Enter":
+      event.preventDefault()
+      equals()
+      break
+    case "Backspace":
+      removeOne()
+      break
+  }
+}
 
 function clearInput() {
   input.innerText = "0"
 }
 
 function previousNumDoesntExist() {
-  previous.innerText = input.innerText
+  previous.innerText = input.innerText || "0"
   previousNum = Number(input.innerText)
 }
 
-function dot() {
+function handleDot() {
   if (input.innerText.includes(".")) return
 
   input.innerText += "."
@@ -145,12 +193,15 @@ function clear() {
   equalsBlocked = false
 }
 
-function numberButtonClicked() {
+function numberButtonClicked(buttonText) {
   if (input.innerText == "0") input.innerText = ""
 
-  input.innerText += this.innerText
+  equalsBlocked = false
+  input.innerText += buttonText
 } 
 
 function removeOne() {
   input.innerText = input.innerText.slice(0, -1)
+
+  if (input.innerText.length == 0) equalsBlocked = true
 }
